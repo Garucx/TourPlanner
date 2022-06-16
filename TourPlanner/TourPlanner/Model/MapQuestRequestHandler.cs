33690e916b002ff837d8502b994992026c3f8a04
Nemanja Adamovic <if20b032@technikum-wien.at>
2022-06-17 01:26:20 +0200

    Versucht Route Image zu downloaden. Nicht geklappt. Viel Glück

--- TourPlanner/TourPlanner/Model/MapQuestRequestHandler.cs
+++ TourPlanner/TourPlanner/Model/MapQuestRequestHandler.cs
@@ -1,10 +1,14 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace TourPlanner.Model
 {
@@ -15,19 +19,31 @@ namespace TourPlanner.Model
          * http://www.mapquestapi.com/directions/v2/route?key=mpUdVgf8ptUM5Bum4hKF2yKU30TlOLw4&from=1150 Jurekgasse,Vienna,AT&to=1150 Arnsteingasse, Vienna, AT
          * https://open.mapquestapi.com/staticmap/v5/map?key=mpUdVgf8ptUM5Bum4hKF2yKU30TlOLw4&start=1150 Jurekgasse,Vienna,AT&end=1150 Arnsteingasse, Vienna, AT
         */
-        public static async Task<Rootobject> GetRouteAsync(MapQuestRequestData start, MapQuestRequestData dest)
+        public static async Task<(Rootobject route ,Bitmap image)> GetRouteAsync(MapQuestRequestData start, MapQuestRequestData dest)
         {
             using var client = new HttpClient();
             var res = await client.GetAsync($"http://www.mapquestapi.com/directions/v2/route?key=mpUdVgf8ptUM5Bum4hKF2yKU30TlOLw4&from={start.GetString()}&to={dest.GetString()}");
 
             var json = await res.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<Rootobject>(json);
+            return (JsonConvert.DeserializeObject<Rootobject>(json),GetRouteImageAsync(start,dest).Result);
             
         }
 
-        public static async Task GetRouteImageAsync(MapQuestRequestData start, MapQuestRequestData dest)
+        public static async Task<Bitmap> GetRouteImageAsync(MapQuestRequestData start, MapQuestRequestData dest)
         {
+            // Dieser Scheiß stürzt das Program ab. Ka wie ich das fixen soll
+            //await Task.Delay(1);
+            //WebClient client = new WebClient();
+            //Stream stream = client.OpenRead("https://open.mapquestapi.com/staticmap/v5/map?key=mpUdVgf8ptUM5Bum4hKF2yKU30TlOLw4&start=1150 Jurekgasse,Vienna,AT&end=1150 Arnsteingasse, Vienna, AT");
+            //Bitmap bitmap; bitmap = new Bitmap(stream);
 
+            //stream.Flush();
+            //stream.Close();
+            //client.Dispose();
+
+            //return bitmap;
+
+            return null;
         }
     }
 }
--- TourPlanner/TourPlanner/obj/Debug/net5.0-windows/View/AddTourDialog.g.i.cs
+++ TourPlanner/TourPlanner/obj/Debug/net5.0-windows/View/AddTourDialog.g.i.cs
@@ -125,7 +125,7 @@ namespace TourPlanner.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/TourPlanner;V1.0.0.0;component/view/addtourdialog.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/TourPlanner;component/view/addtourdialog.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\AddTourDialog.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
